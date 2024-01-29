0ca1e0e5515abd646ada7e34ba0b94dfa06fb6dd
Ankan Saha <ankansahadaluabari@gmail.com>
2024-01-29 15:32:03 +0530

    Added : Added Boolean Generator

--- src/uniquegen.ts
+++ src/uniquegen.ts
@@ -1,43 +1,128 @@
-/* COPYRIGHT BY @AnkanSaha */
-/* COPYRIGHT YEAR 2024 */
+/**
+ * @fileoverview This file contains the implementation of the `Generate` object that provides functions for generating random numbers, words, symbols, mixed strings, and booleans.
+ * @module uniquegen
+ * @license COPYRIGHT BY @AnkanSaha
+ * @license COPYRIGHT YEAR 2024
+ */
 
-// global TypeScript types
-type num = number; // Type for number
-type bool = boolean; // Type for boolean
-type str = string; // Type for string
+/**
+ * Import the Random Number Generator Function.
+ * @internal
+ */
+import GenerateNumberID from './Function/NumFunction';
 
-// Importing the Function from the Function folder
-import GenerateNumberID from './Function/NumFunction'; // Import the Random Number Generator Function
-import GenerateWordID from './Function/WordFunction'; // Import the Random Word Generator Function
-import GenerateSymbolID from './Function/SymbolFunction'; // Import the Random Symbol Generator Function
-import GenerateMixedID from './Function/MixedFunction'; // Import the Random Mixed Generator Function
+/**
+ * Import the Random Word Generator Function.
+ * @internal
+ */
+import GenerateWordID from './Function/WordFunction';
 
-// Global TypeScript Interfaces
-export interface Generate {
-    randomNumber: (length: num, WithZero: bool, CustomNumbers: num[] | undefined) => num;
-    randomWord: (length: num, isCAPITAL: bool, CustomWords: str[] | undefined) => str;
-    randomSymbol: (length: num, CustomSymbols: str[] | undefined) => str;
-    randomMixed: (length: num, isCAPITAL: bool, CustomMixeds: str[] | undefined) => str;
-}
+/**
+ * Import the Random Symbol Generator Function.
+ * @internal
+ */
+import GenerateSymbolID from './Function/SymbolFunction';
 
-/* This code block is creating an object named `Generate` that implements the `Generate` interface. It
-is assigning the four functions `GenerateNumberID`, `GenerateWordID`, `GenerateSymbolID`, and
-`GenerateMixedID` to the properties `randomNumber`, `randomWord`, `randomSymbol`, and `randomMixed`
-respectively. These functions are exported from separate files and are used to generate random
-numbers, words, symbols, and mixed strings. The `Generate` object is then exported as the default
-export of the module. */
-const Generate: Generate = {
-    // Export the synchronous functions
+/**
+ * Import the Random Mixed Generator Function.
+ * @internal
+ */
+import GenerateMixedID from './Function/MixedFunction';
+
+/**
+ * Import the Random Boolean Generator Function.
+ * @internal
+ */
+import GenerateBooleanID from './Function/BooleanFunction';
+
+/**
+ * Import the Generate Interface.
+ * @internal
+ */
+import { Generate } from './Interfaces/uniquegen.interface';
+
+/**
+ * This code block is creating an object named `Generate` that implements the `Generate` interface. It
+ * is assigning the four functions `GenerateNumberID`, `GenerateWordID`, `GenerateSymbolID`, and
+ * `GenerateMixedID` to the properties `randomNumber`, `randomWord`, `randomSymbol`, and `randomMixed`
+ * respectively. These functions are exported from separate files and are used to generate random
+ * numbers, words, symbols, and mixed strings. The `Generate` object is then exported as the default
+ * export of the module.
+ */
+const Generate: Generate = Object.freeze({
+    /**
+     * Generate a random number.
+     * @function
+     * @name Generate.randomNumber
+     * @returns {number} The generated random number.
+     */
     randomNumber: GenerateNumberID,
+
+    /**
+     * Generate a random word.
+     * @function
+     * @name Generate.randomWord
+     * @returns {string} The generated random word.
+     */
     randomWord: GenerateWordID,
+
+    /**
+     * Generate a random symbol.
+     * @function
+     * @name Generate.randomSymbol
+     * @returns {string} The generated random symbol.
+     */
     randomSymbol: GenerateSymbolID,
+
+    /**
+     * Generate a random mixed string.
+     * @function
+     * @name Generate.randomMixed
+     * @returns {string} The generated random mixed string.
+     */
     randomMixed: GenerateMixedID,
-};
 
-export const randomNumber = GenerateNumberID; // Export the Function for Random Number
-export const randomWord = GenerateWordID; // Export the Function for Random Word
-export const randomSymbol = GenerateSymbolID; // Export the Function for Random Symbol
-export const randomMixed = GenerateMixedID; // Export the Function for Random Mixed
+    /**
+     * Generate a random boolean value.
+     * @function
+     * @name Generate.randomBoolean
+     * @returns {boolean} The generated random boolean value.
+     */
+    randomBoolean: GenerateBooleanID
+});
 
-// Export the default function
-export default Generate; // Export the Function in Index
+/**
+ * Export the Function for Random Number.
+ * @public
+ */
+export const randomNumber = GenerateNumberID;
+
+/**
+ * Export the Function for Random Word.
+ * @public
+ */
+export const randomWord = GenerateWordID;
+
+/**
+ * Export the Function for Random Symbol.
+ * @public
+ */
+export const randomSymbol = GenerateSymbolID;
+
+/**
+ * Export the Function for Random Mixed.
+ * @public
+ */
+export const randomMixed = GenerateMixedID;
+
+/**
+ * Export the Function for Random Boolean.
+ * @public
+ */
+export const randomBoolean = GenerateBooleanID;
+
+/**
+ * Export the default function.
+ * @public
+ */
+export default Generate;
